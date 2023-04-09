e8a9d7d60b9bc0b18eae275ea7165a5f687be834
martinfou <martinfou@gmail.com>
2023-04-09 19:38:36 -0400

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import 'zone.js/dist/zone';
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { of, from } from 'rxjs';
+import { of, from, map, tap, take } from 'rxjs';
 
 @Component({
   selector: 'my-app',
@@ -19,19 +19,35 @@ export class App implements OnInit {
   name = 'Angular';
 
   ngOnInit() {
-    of(2, 4, 6, 8).subscribe((item) => console.log(item));
+    // of(2, 4, 6, 8)
+    //   .pipe(map((item) => item * 2))
+    //   .subscribe((x) => console.log(x));
 
-    of('martin', 'david', 'francis').subscribe({
-      next: (item) => console.log(`resulting item .. ${item}`),
-      error: (err) => console.error(`error occured ${err}`),
-      complete: () => console.log('complete'),
-    });
+    // of('martin', 'david', 'francis').subscribe({
+    //   next: (item) => console.log(`resulting item .. ${item}`),
+    //   error: (err) => console.error(`error occured ${err}`),
+    //   complete: () => console.log('complete'),
+    // });
 
-    from([20, 15, 10, 5]).subscribe({
-      next: (item) => console.log(`resulting item .. ${item}`),
-      error: (err) => console.error(`error occured ${err}`),
-      complete: () => console.log('complete'),
-    });
+    from([20, 15, 10, 5])
+      .pipe(
+        tap((item) => console.log(`emitted item ..... ${item}`)),
+        map((item) => item * 2),
+        map((item) => item - 10),
+        map((item) => {
+          if (item === 0) {
+            throw new Error('zero detected');
+          }
+          return item;
+        }),
+        take(2)
+      )
+
+      .subscribe({
+        next: (item) => console.log(`resulting item .. ${item}`),
+        error: (err) => console.error(`error occured ${err}`),
+        complete: () => console.log('complete'),
+      });
   }
 }
 
